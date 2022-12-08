61e4983d3278cdfba794a9087b52bd54b0f6f3a9
Taofeek Ajibade <haysquare93@gmail.com>
2022-12-08 14:02:40 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
-
 /**
- *  * main - Entry point
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
- *    * Return: 0
- *    */
-
+ * main - Entry
+ * Return: Always 0
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}}
+}
