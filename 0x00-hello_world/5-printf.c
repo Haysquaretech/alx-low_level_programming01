a74c8d7604d930312a0cc5837098a3e07ecd0c35
Taofeek Ajibade <haysquare93@gmail.com>
2022-12-08 22:00:55 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
-#include <stdio.h>                                                   /**                                                                                                                                     
- * main - Entry                                                    
- * Return: Always 0                                                  
-*/
+
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * Description: prints "with proper grammar,but the outcome is a piece of art,"
+ *    * Return: 0
+ *    */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+}}
