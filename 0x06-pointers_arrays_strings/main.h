a90f1605cced8f414664f1249d282b2c2597ba58
Taofeek Ajibade <haysquare93@gmail.com>
2022-12-21 22:17:07 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,9 @@ char *string_toupper(char *a);
 char *cap_string(char *a);
 char *leet(char *a);
 int _putchar(char c);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
